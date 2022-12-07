d0283802c358c02777a8a48a4e21aa9099e7dc66
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2022-12-07 18:24:07 -0500

    Complete lab

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 console.log("JS is alive");
+
+// ------------- CASHED ELEMENT RESERVE ---------------------
+const inputEl = document.getElementById('input')
+const btnEl = document.getElementById('submit-button')
+const ulEl = document.getElementById('todo-list')
+// -------------------------------------------
+
+btnEl.addEventListener('click', function(evt){
+  const li = document.createElement('li')
+  const inpEl = document.querySelector('input')
+  li.textContent = inpEl.value
+  console.dir(inpEl);
+  document.querySelector('ul').appendChild(li)
+})
